9416ef590ade7df81412682eb4a1eda787b92fc1
Heiko Scherrer <scherrer@openwms.org>
2015-06-24 21:24:36 +0200

    fixed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,7 +12,6 @@ angular.module('amebaWebApp')
     httpGetRequest = function (path, method, callbackSuccess, callbackError) {
       var xhr = new XMLHttpRequest();
 
-
       xhr.onload = function () {
         if (xhr.readyState === 4) {
           if (xhr.status === 200 || xhr.status === 0) {
@@ -20,17 +19,10 @@ angular.module('amebaWebApp')
             return;
           }
         }
-        var response = {
-          message: 'The resource with path ' + path + ' could not be loaded',
-          messageKey: 'unknown.resource',
-          httpStatus: '404'
-        };
-
-        console.log(xhr.statusText);
-        callbackError(response);
+        callbackError(xhr.response);
       };
 
-      xhr.onerror = function (e) {
+      xhr.addEventListener("error", function (e) {
         var response = {
           message: 'The resource with path ' + path + ' could not be loaded',
           messageKey: 'unknown.resource',
@@ -38,18 +30,20 @@ angular.module('amebaWebApp')
         };
         console.log(xhr.statusText);
         callbackError(response);
-      };
+      }, false);
 
       xhr.open(method, path, true);
-      //xhr.setRequestHeader('Authorization', 'Basic '+ btoa('sva01:P!lot2014SV@'));
-      //xhr.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
+//      xhr.setRequestHeader('Authorization', 'Basic '+ btoa('sva01:P!lot2014SV@'));
+//      xhr.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
       xhr.withCredentials = true;
       xhr.send();
-
     };
-    httpGetRequest("https://www.google.ch/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=test", "GET", function(response) {
-      console.log(response);
-    }, function(response) {
-      console.log("Error");
-    });
+
+    httpGetRequest("http://127.0.0.1:8080/dossiers", "GET",
+        function(response) {
+          console.log(response);
+        }, function(response) {
+          console.log(response);
+        }
+    );
   });
