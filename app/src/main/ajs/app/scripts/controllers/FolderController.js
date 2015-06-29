22717ae74395050ca49627d008b2a6c8bdb3c566
Boris Simic <borochief@gmail.com>
2015-06-29 16:53:38 +0200

    Work on folder tree

--- app/src/main/ajs/app/scripts/controllers/FolderController.js
+++ app/src/main/ajs/app/scripts/controllers/FolderController.js
@@ -3,27 +3,83 @@
 define([
     'app',
     'models/DossierModel',
+    'models/FolderModel',
     'services/CoreService',
     'bootstrap_treeview',
     'material',
     'ripples'
 ], function (app) {
 
-    var ctrl = function ($scope, $translatePartialLoader, $translate, $location, DossierModel, CoreService) {
+    var ctrl = function ($scope, $translatePartialLoader, $translate, $location, $stateParams, DossierModel, FolderModel, DossierService, CoreService) {
 
         $translatePartialLoader.addPart('folder');
         $translate.refresh();
         $translate.use($scope.getUserLang());
 
         /** Model binding for view. */
-        $scope.dossierModel = DossierModel;
+        $scope.folderModel = FolderModel;
 
+        function getTree() {
+
+            var tree = [
+                {
+                    text: $scope.folderModel.selectedDossier.dossierId,
+                    state: {
+                        expanded: true,
+                    },
+                    nodes: [/*
+                        {
+                            text: "Child 1",
+                            nodes: [
+                                {
+                                    text: "Grandchild 1"
+                                },
+                                {
+                                    text: "Grandchild 2"
+                                }
+                            ]
+                        },
+                        {
+                            text: "Child 2"
+                        }
+                    */]
+                }
+            ];
+
+            angular.forEach(FolderModel.selectedDossier.folderModel, function(folder){
+                tree.nodes.add(folder)
+            });
+
+            return tree;
+        };
 
         function init() {
+            if (DossierModel && DossierModel.selectedDossier) {
+
+                // use model to build view ... initialize folderModel
+                $scope.folderModel.selectedDossier = DossierModel.selectedDossier;
+            } else {
+
+                // we come from a reloaded page
+                DossierService.reload($scope).then(
+                    function() {
+                        angular.forEach(DossierModel.dossiers, function(dossier) {
+                            if (dossier.identifier === $stateParams.id) {
+                                $scope.folderModel.selectedDossier = dossier;
+                            }
+                        });
+                    },
+                    function(e) {
+
+                    }
+                );
+            }
+            angular.element('#tree').treeview({data: getTree()});
+            console.dir(DossierModel.selectedDossier);
         }
         init();
     };
 
-    app.register.controller('FolderController', ['$scope', '$translatePartialLoader', '$translate', '$location', 'DossierModel', 'CoreService', ctrl]);
+    app.register.controller('FolderController', ['$scope', '$translatePartialLoader', '$translate', '$location', '$stateParams', 'DossierModel', 'FolderModel', 'DossierService', 'CoreService', ctrl]);
 });
 
